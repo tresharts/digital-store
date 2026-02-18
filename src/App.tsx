f97f2c2a36f6b764f29b7801fd72469dea735208
Gabrielprograme <jgmarques859@gmail.com>
2026-02-17 22:29:00 -0300

    Corrigindo Marcadores

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import { PageLayout } from './layouts/PageLayout';
 import './App.css';
-import { HomePage } from './pages/HomePage';
 import { NotFolder } from './components/NotFolder/NotFolder';
 import { Produtos } from './pages/Produtos';
 import Categoria from './pages/Categorias';
-=======
 import { PageLayout } from '@/layouts/PageLayout';
 import '@/App.css';
 import { HomePage } from '@/pages/HomePage';
-
->>>>>>> 0b8c0544c32e8fcf55ae9fb59f71bf2b9e503015
 const App = () => {
   return (
     <BrowserRouter>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import Apresentacao from "@/components/Apresentacao/Apresentacao";
 import Colecao from "@/components/Colecao/Colecao";
 import Colexao2 from "@/components/Colecao/colecao2";
+import { SpecialOffer } from "@/components/SpecialOffer";
 export const HomePage = () => {
   return (
     <div>
       <Apresentacao/> 
       <Colecao/>
       <Colexao2/>
+      <SpecialOffer/>
     </div>
   );
 }
